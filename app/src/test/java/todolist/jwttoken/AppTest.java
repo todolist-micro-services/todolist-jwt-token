d97ca9b41b966d6f160cf3bfeef2a3e899d30802
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2023-12-07 22:06:10 -0800

    test(expiration date): tests for expiration dates

--- app/src/test/java/todolist/jwttoken/AppTest.java
+++ app/src/test/java/todolist/jwttoken/AppTest.java
@@ -1,11 +1,35 @@
 package todolist.jwttoken;
 
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    @Test void expirationDate1() {
+        String secret = "asdfSFS34wfsdfsdfSDSD32dfsddDDerQSNCK34SOWEK5354fdgdf4";
+        String name = "John Doe";
+        String email = "johndoe@gmail.com";
+        String subject = "John";
+        int nbrHour = 24;
+        JwtTokenType token = JwtToken.createJwtToken(secret, name, email, subject, nbrHour);
+        LocalDateTime tomorrow = LocalDateTime.now().plusHours(24);
+
+        assertEquals(tomorrow.getDayOfMonth(), token.getExpirationDate().getDayOfMonth());
+        assertNotNull(token.getJwtValue());
+    }
+
+    @Test void expirationDate2() {
+        String secret = "asdfSFS34wfsdfsdfSDSD32dfsddDDerQSNCK34SOWEK5354fdgdf4";
+        String name = "John Doe";
+        String email = "johndoe@gmail.com";
+        String subject = "John";
+        int nbrHour = 24;
+        JwtTokenType token = JwtToken.createJwtToken(secret, name, email, subject, nbrHour);
+        LocalDateTime tomorrow = LocalDateTime.now().plusHours(48);
+
+        assertNotEquals(tomorrow.getDayOfMonth(), token.getExpirationDate().getDayOfMonth());
+        assertNotNull(token.getJwtValue());
     }
 }
